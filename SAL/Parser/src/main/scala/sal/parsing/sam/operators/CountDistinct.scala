556db11e319176893ddd45efbe13fbb0357cc588
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-12 20:32:06 -0600

    Update CountDistinct.scala

--- SAL/Parser/src/main/scala/sal/parsing/sam/operators/CountDistinct.scala
+++ SAL/Parser/src/main/scala/sal/parsing/sam/operators/CountDistinct.scala
@@ -1,43 +1,59 @@
 package sal.parsing.sam.operators
 
+import com.typesafe.scalalogging.LazyLogging
+
 import scala.collection.mutable.HashMap
 import sal.parsing.sam.BaseParsing
 import sal.parsing.sam.Constants
 import sal.parsing.sam.Util
 
-trait KMedians extends BaseParsing {
-  val kMediansKeyWord: String = "kmedians"
-
-  def kMediansOperator: Parser[KMediansExp] =
-    kMediansKeyWord ~ "(" ~ identifier ~ "," ~ int ~ ")" ^^
-      { case kmed ~ lpar ~ id ~ c1 ~ k ~ rpar =>
-        KMediansExp(id, k, memory)
-      }
+trait CountDistinct extends BaseParsing with LazyLogging
+{
+  def countDistinctOperator : Parser[CountDistinctExp] = 
+    // When the parameters are not specified
+    countDistinctKeyWord ~ "(" ~ identifier ~ ")" ^^
+    {case cm ~ lpar ~ id ~ rpar =>
+      val windowSize = memory.getOrElse(Constants.WindowSize,
+        Constants.DefaultWindowSize).toInt;
+      CountDistinctExp(id, windowSize, memory)} |
+    // When the parameters are specified
+    countDistinctKeyWord ~ "(" ~ identifier ~ "," ~ posInt ~ ")" ^^
+    {case cm ~ lpar ~ id ~ c1 ~ n ~ rpar =>
+      CountDistinctExp(id, n, memory)}
 }
 
-case class KMediansExp(field: String, k: Int, memory: HashMap[String, String])
-    extends OperatorExp(field, memory) with Util {
+/**
+ * An expression of the form CountDistinct(field, N).
+ * @param field This is a keyword of the high-level language
+ *   indicating what field where count distinct is being calculated.
+ * @param N The number of items in the window
+ */
+case class CountDistinctExp(field: String, N: Int, 
+                          memory: HashMap[String, String])
+  extends OperatorExp(field, memory) with Util with LazyLogging
+{
+  override def createOpString() =
+  {
+    logger.info("CountDistinctExp.createOpString")
+    val lstream = memory.get(Constants.CurrentLStream).get
+    val rstream = memory.get(Constants.CurrentRStream).get
 
-  override def createOpString(): String = {
-    val lstream = memory.getOrElse(Constants.CurrentLStream, "")
-    val rstream = memory.getOrElse(Constants.CurrentRStream, "")
+    // Getting fields that are arguments of the template`
+    val tupleType = memory.get(lstream + Constants.TupleType).get
+    val numKeys = memory.get(lstream + Constants.NumKeys).get
+    var keysString = ""
+    for (i <- 0 until numKeys.toInt ) {
+      keysString = keysString +
+        memory.get(lstream + Constants.KeyStr + i).get + ", "
+    }
+    keysString = keysString.dropRight(2)
 
-    // Generate SAM code for the K-medians operator
-    val opString = s"""identifier = "$field";
-    auto $field = std::make_shared<KMedians<>>($k);
-    ${addRegisterStatements(field, rstream, memory)}"""
-
-    opString
-  }
-
-  override def addRegisterStatements(identifier: String, rstream: String, memory: HashMap[String, String]): String = {
-    val producer = "producer" // Replace with the appropriate producer object
-    val subscriber = "subscriber" // Replace with the appropriate subscriber object
-
-    s"""addOperator($identifier);
-    registerConsumer($identifier, "$identifier");
-    if ($subscriber != NULL) {
-      $producer->registerSubscriber($subscriber, $identifier);
-    }"""
+    var rString = "  identifier = \"" + lstream + "\";\n"
+    rString += "  auto " + lstream +
+      " = std::make_shared<CountDistinct<\n" +
+      "    size_t, EdgeType, " + field + ", " + keysString + ">>(" +
+      N.toString + ", nodeId, featureMap, identifier);\n"
+    rString += addRegisterStatements(lstream, rstream, memory)
+    rString
   }
 }
