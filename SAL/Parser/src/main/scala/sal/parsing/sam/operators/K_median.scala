52c7fc41cbf9247b5c8b3828ae3f4c9974ecd00f
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-18 20:36:34 -0600

    Update K_median.scala

--- SAL/Parser/src/main/scala/sal/parsing/sam/operators/K_median.scala
+++ SAL/Parser/src/main/scala/sal/parsing/sam/operators/K_median.scala
@@ -19,21 +19,21 @@ case class KMediansExp(field: String, k: Int, memory: HashMap[String, String])
     extends OperatorExp(field, memory) with Util {
 
     override def createOpString(): String = {
-    val lstream = memory.getOrElse(Constants.CurrentLStream, "")
-    val rstream = memory.getOrElse(Constants.CurrentRStream, "")
+  val lstream = memory.getOrElse(Constants.CurrentLStream, "")
+  val rstream = memory.getOrElse(Constants.CurrentRStream, "")
 
-    // Get the tuple type of the input stream
-  
-    val tupleType = memory.getOrElse(lstream + Constants.TupleType, "")
-    memory += Constants.TupleType -> tupleType
+  // Get the tuple type of the input stream
+  val tupleType = memory.getOrElse(lstream + Constants.TupleType, "")
+  memory += Constants.TupleType -> tupleType
 
-    // Generate SAM code for the K-medians operator
-    val opString = s"""identifier = "$field";
+  // Generate SAM code for the K-medians operator
+  val opString = s"""identifier = "$field";
 auto $field = std::make_shared<KMedians<$tupleType>>($k);
-${addRegisterStatements(field, rstream, memory)}"""
+${addRegisterStatements(field, rstream, memory)}""".replace("$tupleType", tupleType)
 
-    opString
-  }
+  opString
+}
+
 
   override def addRegisterStatements(identifier: String, rstream: String, memory: HashMap[String, String]): String = {
     val producer = "producer" // Replace with the appropriate producer object
