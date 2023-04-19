5f1a0c0b0aa0d8997201f9b81bf043cca3373f35
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-18 20:46:17 -0600

    Update K_median_test.scala

--- SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
+++ SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
@@ -25,22 +25,7 @@ class KMediansSpec extends FlatSpec with KMedians {
   }
 
 
-    it must "generate correct C++ code" in {
-    val input = "kmedians(Stream1, 5)"
-    val expectedOutput = """|identifier = "Stream1";
-                            |auto Stream1 = std::make_shared<KMedians<$tupleType>>(5);
-                            |addOperator(Stream1);
-                            |registerConsumer(Stream1, "Stream1");
-                            |if (subscriber != NULL) {
-                            |  producer->registerSubscriber(subscriber, Stream1);
-                            |}""".stripMargin
 
-    val parsedResult = parseAll(kMediansOperator, input)
-    assert(parsedResult.successful)
-    assert(parsedResult.get.createOpString() == expectedOutput)
-  }
-
-  
   
   
   it must "generate correct C++ code" in {
