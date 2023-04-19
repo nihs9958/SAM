b287780a7327734d3903366e7839631906c93ef8
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-18 20:09:29 -0600

    Update K_median_test.scala

--- SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
+++ SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
@@ -24,17 +24,23 @@ class KMediansSpec extends FlatSpec with KMedians {
     assert(parsedResult.isInstanceOf[Failure])
   }
 
-  it must "generate correct C++ code" in {
+  
+  
+    it must "generate correct C++ code" in {
     val input = "kmedians(Stream1, 5)"
-    val expectedOutput = """  identifier = "Stream1";
-    auto Stream1 = std::make_shared<KMedians<$tupleType>>(5);
-    addOperator(Stream1);
-    registerConsumer(Stream1, "Stream1");"""
+    val expectedOutput = """|identifier = "Stream1";
+                            |auto Stream1 = std::make_shared<KMedians<$tupleType>>(5);
+                            |addOperator(Stream1);
+                            |registerConsumer(Stream1, "Stream1");
+                            |if (subscriber != NULL) {
+                            |  producer->registerSubscriber(subscriber, Stream1);
+                            |}""".stripMargin
 
     val parsedResult = parseAll(kMediansOperator, input)
     assert(parsedResult.successful)
     assert(parsedResult.get.createOpString() == expectedOutput)
   }
+
 
   it must "generate C++ code with the correct identifier" in {
     val input = "kmedians(MyStream, 5)"
