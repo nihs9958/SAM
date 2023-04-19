994fd9e80933e360ae5447ee23e1d89cc3cab6d9
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-04-18 19:51:15 -0600

    Update K_median_test.scala

--- SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
+++ SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
@@ -5,13 +5,13 @@ import sal.parsing.sam.Constants
 class KMediansSpec extends FlatSpec with KMedians {
   "A k-medians operator" must "parse correctly with valid input" in {
     val input = "kmedians(Stream1, 5)"
-    val expectedOutput = """identifier = "Stream1";
-auto Stream1 = std::make_shared<KMedians<>>(5);
-addOperator(Stream1);
-registerConsumer(Stream1, "Stream1");
-if (subscriber != NULL) {
-  producer->registerSubscriber(subscriber, Stream1);
-}"""
+    val expectedOutput = """|identifier = "Stream1";
+                            |auto Stream1 = std::make_shared<KMedians<>>(5);
+                            |addOperator(Stream1);
+                            |registerConsumer(Stream1, "Stream1");
+                            |if (subscriber != NULL) {
+                            |  producer->registerSubscriber(subscriber, Stream1);
+                            |}""".stripMargin
 
     val parsedResult = parseAll(kMediansOperator, input)
     assert(parsedResult.successful)
@@ -38,13 +38,13 @@ if (subscriber != NULL) {
 
   it must "generate C++ code with the correct identifier" in {
     val input = "kmedians(MyStream, 5)"
-    val expectedOutput = """identifier = "MyStream";
-auto MyStream = std::make_shared<KMedians<>>(5);
-addOperator(MyStream);
-registerConsumer(MyStream, "MyStream");
-if (subscriber != NULL) {
-  producer->registerSubscriber(subscriber, MyStream);
-}"""
+    val expectedOutput = """|identifier = "MyStream";
+                            |auto MyStream = std::make_shared<KMedians<>>(5);
+                            |addOperator(MyStream);
+                            |registerConsumer(MyStream, "MyStream");
+                            |if (subscriber != NULL) {
+                            |  producer->registerSubscriber(subscriber, MyStream);
+                            |}""".stripMargin
 
     val parsedResult = parseAll(kMediansOperator, input)
     assert(parsedResult.successful)
