88294ccbd965d5096125e3ae582091f06293eec8
nihs9958 <77692664+nihs9958@users.noreply.github.com>
2023-05-04 02:02:45 -0600

    Update K_median_test.scala

--- SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
+++ SAL/Parser/src/test/scala/sal/parsing/sam/operators/K_median_test.scala
@@ -3,60 +3,65 @@ import sal.parsing.sam.operators.KMedians
 import sal.parsing.sam.Constants
 
 class KMediansSpec extends FlatSpec with KMedians {
-  "A k-medians operator" must "parse correctly with valid input" in {
-    val input = "kmedians(Stream1, 5)"
-    val expectedOutput = """|identifier = "Stream1";
-                            |auto Stream1 = std::make_shared<KMedians<>>(5);
-                            |addOperator(Stream1);
-                            |registerConsumer(Stream1, "Stream1");
-                            |if (subscriber != NULL) {
-                            |  producer->registerSubscriber(subscriber, Stream1);
-                            |}""".stripMargin
 
-    val parsedResult = parseAll(kMediansOperator, input)
-    assert(parsedResult.successful)
-    assert(parsedResult.get.createOpString() == expectedOutput)
+  "A k-medians operator" must "parse a valid input string" in {
+    memory.clear()
+    memory += Constants.CurrentLStream -> "stream1"
+    memory += Constants.CurrentRStream -> "stream2"
+    memory += "stream1" + Constants.TupleType -> "TupleType1"
+    memory += "stream2" + Constants.TupleType -> "TupleType2"
+    memory += "field1" + Constants.TupleType -> "TupleType1"
+    memory += "field1" + Constants.NumKeys -> 1.toString
+    memory += "field1" + Constants.KeyStr + 0 -> "key1"
+    val input = "kmedians(field1, 5)"
+    assert(parseAll(kMediansOperator, input).successful)
   }
 
-  it must "fail to parse with invalid input" in {
-    val input = "kmedians(Stream1, invalid)"
-    val parsedResult = parseAll(kMediansOperator, input)
-    assert(parsedResult.isInstanceOf[Failure])
+  it must "fail to parse an invalid input string" in {
+    memory.clear()
+    memory += Constants.CurrentLStream -> "stream1"
+    memory += Constants.CurrentRStream -> "stream2"
+    memory += "stream1" + Constants.TupleType -> "TupleType1"
+    memory += "stream2" + Constants.TupleType -> "TupleType2"
+    memory += "field1" + Constants.TupleType -> "TupleType1"
+    memory += "field1" + Constants.NumKeys -> 1.toString
+    memory += "field1" + Constants.KeyStr + 0 -> "key1"
+    val input = "kmedians(field1, -1)"
+    assert(parseAll(kMediansOperator, input).isEmpty)
   }
 
-
-
-  
-  
   it must "generate correct C++ code" in {
-  val input = "kmedians(Stream1, 5)"
-  val expectedOutput = """|identifier = "Stream1";
-                          |auto Stream1 = std::make_shared<KMedians<>>(5);
-                          |addOperator(Stream1);
-                          |registerConsumer(Stream1, "Stream1");
-                          |if (subscriber != NULL) {
-                          |  producer->registerSubscriber(subscriber, Stream1);
-                          |}""".stripMargin
+    memory.clear()
+    memory += Constants.CurrentLStream -> "stream1"
+    memory += Constants.CurrentRStream -> "stream2"
+    memory += "stream1" + Constants.TupleType -> "TupleType1"
+    memory += "stream2" + Constants.TupleType -> "TupleType2"
+    memory += "field1" + Constants.TupleType -> "TupleType1"
+    memory += "field1" + Constants.NumKeys -> 1.toString
+    memory += "field1" + Constants.KeyStr + 0 -> "key1"
+    val field = "field1"
+    val k = 5
+    val expectedOutput = s"""identifier = "$field";
+auto $field = std::make_shared<KMedians<TupleType1>>($k);
+${addRegisterStatements(field, "stream2", memory)}"""
+    val actualOutput = KMediansExp(field, k, memory).createOpString()
+    assert(actualOutput.trim == expectedOutput.trim)
+  }
 
-  val parsedResult = parseAll(kMediansOperator, input)
-  assert(parsedResult.successful)
-  assert(parsedResult.get.createOpString() == expectedOutput)
-}
-
-  
-  
   it must "generate C++ code with the correct identifier" in {
-    val input = "kmedians(MyStream, 5)"
-    val expectedOutput = """|identifier = "MyStream";
-                            |auto MyStream = std::make_shared<KMedians<>>(5);
-                            |addOperator(MyStream);
-                            |registerConsumer(MyStream, "MyStream");
-                            |if (subscriber != NULL) {
-                            |  producer->registerSubscriber(subscriber, MyStream);
-                            |}""".stripMargin
-
-    val parsedResult = parseAll(kMediansOperator, input)
-    assert(parsedResult.successful)
-    assert(parsedResult.get.createOpString() == expectedOutput)
+    memory.clear()
+    memory += Constants.CurrentLStream -> "stream1"
+    memory += Constants.CurrentRStream -> "stream2"
+    memory += "stream1" + Constants.TupleType -> "TupleType1"
+    memory += "stream2" + Constants.TupleType -> "TupleType2"
+    memory += "field1" + Constants.TupleType -> "TupleType1"
+    memory += "field1" + Constants.NumKeys -> 1.toString
+    memory += "field1" + Constants.KeyStr + 0 -> "key1"
+    val field = "field1"
+    val k = 5
+    val expectedOutput = s"""identifier = "$field";"""
+    val actualOutput = KMedians
+    Exp(field, k, memory).createOpString()
+    assert(actualOutput.trim.startsWith(expectedOutput.trim))
   }
 }
